084009b8d02d1839752645fdf645670c64b085f5
Mikolaj Zawada <m.zawada@bsgroup.eu>
2024-05-09 10:02:53 +0200

    Initial Commit

---
+++ JakDojadeRek/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JakDojadeRek
+//
+//  Created by Mikolaj Zawada on 09/05/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
